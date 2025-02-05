3e40190ff23342847e7f4ad8df8d954eb975d424
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2025-02-05 16:56:35 +0000

    Fix function page returning 404

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));  // This auto-adds public/index.html to the "/" page
-app.use(require("./server/page.js"));
-app.use(require("./server/error.js"));
-
 
 function function1(id, reps)
 {
@@ -24,6 +19,10 @@ app.get(`/function1/:id/:reps`, (req, res) => {
     res.send(concat);
 })
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));  // This auto-adds public/index.html to the "/" page
+app.use(require("./server/page.js"));
+app.use(require("./server/error.js"));
 
 // Start the server
 app.listen(PORT, () => {
