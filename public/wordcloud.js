41f25ea4624b468f283bf6bfd6c8900b3687dd86
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2025-02-10 11:28:27 +0000

    Move wordcloud api to its own file

---
+++ public/wordcloud.js
@@ -0,0 +1,35 @@
+
+// Personalised greeting
+document.getElementById('APISForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Prevent page reload
+    
+    const topic = document.getElementById('topicInput').value;
+    // Convert the object to a JSON string
+    const jsonString = JSON.stringify({"topic": topic});
+
+    // // Send the JSON string to the server
+    await fetch('/API1', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: jsonString // Send the JSON string directly
+    })
+        .then(async (res) => {
+            const imgElement = document.getElementById('imageElement')
+
+            // Checks to see if response is of the correct MIME type - PNG
+            const contentType = res.headers.get('Content-Type');
+            if (!contentType || !contentType.includes('image/png')) {
+                throw new Error('The server did not return a PNG image');
+            }
+
+            // Creates a blob from the response buffer
+            //   Gets the url for the blob
+            const buffer = await res.arrayBuffer();
+            const blob = new Blob([buffer], { type: 'image/png' });
+            const url = URL.createObjectURL(blob);
+
+            imgElement.src = url;  // Prefix with appropriate MIME type
+            imgElement.style.display = 'block'; // Show image once loaded
+        })
+});
+
